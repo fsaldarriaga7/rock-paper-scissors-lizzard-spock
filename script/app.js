a16e9165bad685d040a94b864bc9d446875871ff
Felipe Saldarriaga <saldarriagafelipe3@gmail.com>
2022-03-08 11:36:09 -0500

    Agregado Current Streak

--- script/app.js
+++ script/app.js
@@ -5,10 +5,12 @@ const userChoiceDisplay = document.getElementById('user-choice');
 
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
+const currentStreakDisplay = document.getElementById('current-streak');
 
 let userChoice;
 let computerChoice;
 let result;
+let streak = 0;
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (event) => {
     userChoice = event.target.id;
@@ -47,67 +49,88 @@ function getResult() {
     }
     //Computer Wins
     if (computerChoice === 'Rock' && userChoice === 'Scissors'){
-        result = 'Computer Wins';     
+        result = 'Computer Wins';
+        streak--;     
     }
     if (computerChoice === 'Rock' && userChoice === 'Lizzard'){
-        result = 'Computer Wins';        
+        result = 'Computer Wins';
+        streak--;     
     }
     if (computerChoice === 'Paper' && userChoice === 'Rock'){
-        result = 'Computer Wins';        
+        result = 'Computer Wins';     
+        streak--;   
     }
     if (computerChoice === 'Paper' && userChoice === 'Spock'){
-        result = 'Computer Wins';        
+        result = 'Computer Wins';
+        streak--;        
     }
     if (computerChoice === 'Scissors' && userChoice === 'Paper'){
-        result = 'Computer Wins';        
+        result = 'Computer Wins';  
+        streak--;      
     }
     if (computerChoice === 'Scissors' && userChoice === 'Lizzard'){
-        result = 'Computer Wins';       
+        result = 'Computer Wins';
+        streak--;       
     }
     if (computerChoice === 'Lizzard' && userChoice === 'Paper'){
-        result = 'Computer Wins';        
+        result = 'Computer Wins';
+        streak--;        
     }
     if (computerChoice === 'Lizzard' && userChoice === 'Spock'){
-        result = 'Computer Wins';        
+        result = 'Computer Wins';
+        streak--;        
     }
     if (computerChoice === 'Spock' && userChoice === 'Scissors'){
-        result = 'Computer Wins';        
+        result = 'Computer Wins';
+        streak--;        
     }
     if (computerChoice === 'Spock' && userChoice === 'Rock'){
-        result = 'Computer Wins';        
+        result = 'Computer Wins'; 
+        streak--;       
     }
     //You Win
     if (computerChoice === 'Rock' && userChoice === 'Paper'){
-        result = 'You Win';        
+        result = 'You Win'; 
+        streak++;       
     }
     if (computerChoice === 'Rock' && userChoice === 'Spock'){
-        result = 'You Win';        
+        result = 'You Win'; 
+        streak++;        
     }
     if (computerChoice === 'Scissors' && userChoice === 'Rock'){
-        result = 'You Win';        
+        result = 'You Win';   
+        streak++;      
     }
     if (computerChoice === 'Scissors' && userChoice === 'Spock'){
-        result = 'You Win';        
+        result = 'You Win';  
+        streak++;       
     }
     if (computerChoice === 'Paper' && userChoice === 'Scissors'){
-        result = 'You Win';        
+        result = 'You Win';  
+        streak++;      
     }
     if (computerChoice === 'Paper' && userChoice === 'Lizzard'){
-        result = 'You Win';        
+        result = 'You Win'; 
+        streak++;       
     }
     if (computerChoice === 'Lizzard' && userChoice === 'Scissors'){
-        result = 'You Win';        
+        result = 'You Win'; 
+        streak++;       
     }
     if (computerChoice === 'Lizzard' && userChoice === 'Rock'){
-        result = 'You Win';        
+        result = 'You Win'; 
+        streak++;       
     }
     if (computerChoice === 'Spock' && userChoice === 'Lizzard'){
-        result = 'You Win';        
+        result = 'You Win';
+        streak++;        
     }
     if (computerChoice === 'Spock' && userChoice === 'Paper'){
-        result = 'You Win';        
+        result = 'You Win';  
+        streak++;      
     }
 
 
     resultDisplay.innerHTML = result;
+    currentStreakDisplay.innerHTML = streak;
 }
